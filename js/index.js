db44139f4e47381f8730a9f013b35462aae14936
Icfesparadisciplinados <153673146+Icfesparadisciplinados@users.noreply.github.com>
2024-01-28 12:07:12 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,25 +11,20 @@ const fechaData = sessionStorage.getItem("fecha")
 const creadorData = sessionStorage.getItem("creador")
 const precioData = sessionStorage.getItem("precio")     
 
-window.addEventListener('beforeunload', async function(event) {
-  event.preventDefault();
-  try {
-    await setLogueado({
-      documento: documentoData,
-      contrasena: contrasenaData,
-      nombre: nombreData,
-      fecha: fechaData,
-      creador: creadorData,
-      precio: precioData,
-      logueado: false,
-    }, "1004631085");
-    console.log('Operación asincrónica completada antes de cerrar la página.');
-  } catch (error) {
-    console.error('Error al realizar la operación asincrónica:', error);
-  }
-  var mensaje = '¿Seguro que quieres salir de la página?';
-  event.returnValue = mensaje;
-  return mensaje;
+window.addEventListener('beforeunload', async (event) => {
+  // La función `setLogueado()` se ejecutará de forma asíncrona
+  await setLogueado({
+    documento: documentoData,
+    contrasena: contrasenaData,
+    nombre: nombreData,
+    fecha: fechaData,
+    creador: creadorData,
+    precio: precioData,
+    logueado: false,
+  }, "1004631085");
+
+  // Personalizar o suprimir el mensaje de confirmación
+  event.returnValue = '';
 });
 
 const masOpcBot = document.querySelector(".masOpcBot")
