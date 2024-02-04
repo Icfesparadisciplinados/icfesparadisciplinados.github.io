87d27d286dddb3c3b154cc8c7256ac884ae6ce52
Icfesparadisciplinados <153673146+Icfesparadisciplinados@users.noreply.github.com>
2024-02-04 12:03:30 -0500

    Delete js directory

--- js/masOpc/desarrolladores.js
+++
@@ -1,9 +0,0 @@
-const back = document.querySelector(".back");
-
-back.addEventListener("mouseenter", function() {
-  back.src = "/img/backHV.svg"
-});
-
-back.addEventListener("mouseleave", function() {
-  back.src = "/img/back.svg"
-});
